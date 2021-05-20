7b08cd0a5b7a07c53a7351f09736e68ac32fd780
Levickiy-hub <55253795+Levickiy-hub@users.noreply.github.com>
2021-05-20 10:19:16 +0300

    Add files via upload

--- public/javascripts/wsClient.js
+++ public/javascripts/wsClient.js
@@ -1,15 +1,15 @@
-﻿function startWS() {
-    let webSocket = new WebSocket('ws://localhost:5000/')
-    function sendWSMessage(content) {
-        webSocket.send(JSON.stringify({
-            content: content
-        }));
-    }
+﻿//function startWS() {
+//    let webSocket = new WebSocket('ws://localhost:5000/')
+//    function sendWSMessage(content) {
+//        webSocket.send(JSON.stringify({
+//            content: content
+//        }));
+//    }
 
-    webSocket.onopen = e => {
-        webSocket.onmessage = function (e) {
-            console.log('wsClient');
-            { html: "<script type='text/javascript'>alert('" + content + " ')</script>" };
-        }
-    };
-}
+//    webSocket.onopen = e => {
+//        webSocket.onmessage = function (e) {
+//            console.log('wsClient');
+//            { html: "<script type='text/javascript'>alert('" + content + " ')</script>" };
+//        }
+//    };
+//}
--- ws/ws.js
+++ ws/ws.js
@@ -1,26 +1,26 @@
-﻿const WebSocket = require('ws')
+﻿//const WebSocket = require('ws')
 
-const wsServer = new WebSocket.Server({ port: 5000, host: 'localhost', path: '/broadcast' })
-let url = require('url');
+//const wsServer = new WebSocket.Server({ port: 5000, host: 'localhost', path: '/' })
+//let url = require('url');
 
-wsServer.on('connection', (ws, req) => {
-    let parsedUrl = url.parse(req.url, true);
+//wsServer.on('connection', (ws, req) => {
+//    let parsedUrl = url.parse(req.url, true);
 
-    //if (parsedUrl.query.id) {
-    //    ws.userId = parseInt(parsedUrl.query.id);
-    //} else {
-    //    ws.close();
-    //}
+//    //if (parsedUrl.query.id) {
+//    //    ws.userId = parseInt(parsedUrl.query.id);
+//    //} else {
+//    //    ws.close();
+//    //}
 
-    ws.on('message', (message) => {
-        let messageObject = JSON.parse(message);
-        wsServer.clients.forEach(client => {
-            if (client.userId === messageObject.to) {
-                client.send(messageObject.content);
-            }
-        });
-    });
-});
+//    ws.on('message', (message) => {
+//        let messageObject = JSON.parse(message);
+//        wsServer.clients.forEach(client => {
+//            if (client.userId === messageObject.to) {
+//                client.send(messageObject.content);
+//            }
+//        });
+//    });
+//});
 
-module.exports = wsServer;
+//module.exports = wsServer;
 
